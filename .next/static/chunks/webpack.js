14e45322ffe3eb862c0b9a51af8a5e2a5ece18ed
PreethiChandran <preethychandran21@gmail.com>
2024-09-01 23:06:06 +0400

    Fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f5522204582e1c2"; }
+/******/ 		__webpack_require__.h = function() { return "def49367cfd46c2c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
